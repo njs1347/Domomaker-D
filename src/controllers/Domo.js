f8f5da64049fd1cc805241ea4fde967796558e4c
Nyrih Smith <njs1347@rit.edu>
2016-04-02 00:32:40 -0400

    body color

--- src/controllers/Domo.js
+++ src/controllers/Domo.js
@@ -18,7 +18,7 @@ var makerPage = function(req, res) {
 
 var makeDomo = function(req, res) {
 	
-	if(!req.body.name || !req.body.age) {
+	if(!req.body.name || !req.body.age || !req.body.color) {
 		return res.status(400).json({error: "RAWR! Both name and age are required"});
 	}
 	
