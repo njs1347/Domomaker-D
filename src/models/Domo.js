094a60d3c6bf87fc5a2084307e716d3db6950f0a
Nyrih Smith <njs1347@rit.edu>
2016-04-01 23:35:27 -0400

    more changes

--- src/models/Domo.js
+++ src/models/Domo.js
@@ -51,7 +51,8 @@ DomoSchema.methods.toAPI = function() {
 DomoSchema.statics.findByOwner = function(ownerId, callback) {
 	
 	var search = {
-		owner: mongoose.Types.ObjectId(ownerId)
+		//owner: mongoose.Types.ObjectId(ownerId)
+		owner: ownerId
 	};
 	
 	return DomoModel.find(search).select("name age color").exec(callback);
